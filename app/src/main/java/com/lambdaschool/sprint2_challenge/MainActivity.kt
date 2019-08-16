debdccdfe6a27ef99c78d9852f04a78fbe8afced
Patrick Chow <PatrickChow0803@gmail.com>
2019-08-16 12:53:08 -0400

    Created the RecyclerView and added the adapter and linear layout to it.

--- app/src/main/java/com/lambdaschool/sprint2_challenge/MainActivity.kt
+++ app/src/main/java/com/lambdaschool/sprint2_challenge/MainActivity.kt
@@ -2,6 +2,8 @@ package com.lambdaschool.sprint2_challenge
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +14,10 @@ class MainActivity : AppCompatActivity() {
 
         ShoppingItemRepository.createShoppingItemList()
 
+        rv_shopping_list.apply{
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = ShoppingListAdapter(ShoppingItemRepository.shoppingItemList)
+        }
     }
 }
